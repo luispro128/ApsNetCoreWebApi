c0921067b96280d315ea3dd6a3ef43b9df58f60a
luispro128 <figueiredo128@hotmail.com>
2021-09-30 22:46:57 -0300

    Models

--- SmartSchoolApp/src/app/app.module.ts
+++ SmartSchoolApp/src/app/app.module.ts
@@ -1,16 +1,52 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AlunosComponent } from './components/alunos/alunos.component';
+import { ProfessoresComponent } from './components/professores/professores.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { TituloComponent } from './components/shared/titulo/titulo.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
+import { ToastrModule} from 'ngx-toastr';
+import { NgxSpinnerModule} from 'ngx-spinner';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlunosComponent,
+    ProfessoresComponent,
+    PerfilComponent,
+    DashboardComponent,
+    NavComponent,
+    TituloComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    ModalModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    NgxSpinnerModule,
+    ToastrModule.forRoot({
+      timeOut:3500,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+      progressBar: true,
+      closeButton: true
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ SmartSchoolApp/src/app/components/alunos/alunos.component.ts
@@ -0,0 +1,145 @@
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { Aluno } from 'src/app/models/Aluno';
+import { PaginatedResult, Pagination } from 'src/app/models/Pagination';
+import { Professor } from 'src/app/models/Professor';
+import { AlunoService } from 'src/app/services/aluno.service';
+import { ProfessorService } from 'src/app/services/professor.service';
+
+@Component({
+  selector: 'app-alunos',
+  templateUrl: './alunos.component.html',
+  styleUrls: ['./alunos.component.css']
+})
+export class AlunosComponent implements OnInit {
+
+  public modalRef!: BsModalRef;
+  public alunoForm!: FormGroup;
+  public titulo = 'Alunos';
+  public alunoSelecionado: Aluno = new Aluno;
+  public textSimple!: string;
+  public profsAlunos: Professor[] = [];
+
+  private unsubscriber = new Subject();
+
+  public alunos: Aluno[] = [];
+  public aluno: Aluno = new Aluno;
+  public msnDeleteAluno!: string;
+  public modeSave = 'post';
+
+  openModal(template: TemplateRef<any>, alunoId: number) {
+    this.professoresAlunos(template, alunoId);
+  }
+
+  closeModal() {
+    this.modalRef.hide();
+  }
+
+  professoresAlunos(template: TemplateRef<any>, id: number) {
+    this.spinner.show();
+    this.professorService.getByAlunoId(id)
+      .pipe(takeUntil(this.unsubscriber))
+      .subscribe((professores: Professor[]) => {
+        this.profsAlunos = professores;
+        this.modalRef = this.modalService.show(template);
+      }, (error: any) => {
+        this.toastr.error(`erro: ${error}`);
+        console.log(error);
+      }, () => this.spinner.hide()
+    );
+  }
+
+  constructor(
+    private alunoService: AlunoService,
+    private route: ActivatedRoute,
+    private professorService: ProfessorService,
+    private fb: FormBuilder,
+    private modalService: BsModalService,
+    private toastr: ToastrService,
+    private spinner: NgxSpinnerService
+  ) {
+    this.criarForm();
+  }
+
+  ngOnInit() {
+    this.carregarAlunos();
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscriber.next();
+    this.unsubscriber.complete();
+  }
+
+  criarForm() {
+    this.alunoForm = this.fb.group({
+      id: [0],
+      nome: ['', Validators.required],
+      sobrenome: ['', Validators.required],
+      telefone: ['', Validators.required]
+    });
+  }
+
+  saveAluno() {
+    if (this.alunoForm.valid) {
+      this.spinner.show();
+
+      if (this.modeSave === 'post') {
+        this.aluno = {...this.alunoForm.value};
+      } else {
+        this.aluno = {id: this.alunoSelecionado.id, ...this.alunoForm.value};
+      }
+
+      this.alunoService[this.modeSave](this.aluno)
+        .pipe(takeUntil(this.unsubscriber))
+        .subscribe(
+          () => {
+            this.carregarAlunos();
+            this.toastr.success('Aluno salvo com sucesso!');
+          }, (error: any) => {
+            this.toastr.error(`Erro: Aluno não pode ser salvo!`);
+            console.error(error);
+          }, () => this.spinner.hide()
+        );
+
+    }
+  }
+
+  carregarAlunos() {
+    const id = +this.route.snapshot.paramMap.get('id');
+
+
+    this.spinner.show();
+    this.alunoService.getAll()
+      .pipe(takeUntil(this.unsubscriber))
+      .subscribe((alunos: Aluno[]) => {
+        this.alunos = alunos;
+
+        if (id > 0) {
+          this.alunoSelect(this.alunos.find(aluno => aluno.id === id));
+        }
+
+        this.toastr.success('Alunos foram carregado com Sucesso!');
+      }, (error: any) => {
+        this.toastr.error('Alunos não carregados!');
+        console.log(error);
+      }, () => this.spinner.hide()
+    );
+  }
+
+  alunoSelect(aluno: Aluno) {
+    this.modeSave = 'put';
+    this.alunoSelecionado = aluno;
+    this.alunoForm.patchValue(aluno);
+  }
+
+  voltar() {
+    this.alunoSelecionado = null;
+  }
+
+}
